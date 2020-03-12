86428f19a060ccb8cea724e7b166e1ab8c23f927
ravi <ravisaini9900@gmail.com>
2020-03-12 15:05:44 +0530

    add chat and about

--- resources/views/abouts.blade.php
+++ resources/views/abouts.blade.php
@@ -3,21 +3,16 @@
 </head>
 <body>
 <div style="margin-left:15px;">
-    <h2 align="center" >Abouts</h2>
-    <h4>What is packagingsapre.com?</h4>
-    Packaging spare is an online portal for packaging machine's quality spare parts and accessories. How many times has
-    it happened with you that the packaging machine's spare parts you are looking for is not available and higher costly
-    in the local spare part market? This is why we brought you packagingspare.com, your one stop shop for buying all the
-    packaging machine's spare parts and accessories. We bring the best aftermarket spare parts to you from reputed
-    brands.
-    <h4>Our aim</h4>
-    Our aim is to bring more transparency in this majorly unorganized sector. We at packagingspare.com are the pioneers
-    of the packaging industry and have been in this industry for a long time; therefore, we are able to procure parts at
-    the lowest cost from our partner brands. We don't even try to inflate prices in the name of “Free Shipping” as our
-    utmost priority lies in the simplicity of the whole process and transparency for complete customer satisfaction.<br><br>
-    The commitments made to our Customers, as they are our first priority. We at packagingspare.com strive to gain the
-    customer's trust by not only providing them with good quality products, but also make them aware of the application,
-    use and care of each packaging machine. Ultimately, we want to take away the hassles of maintenance.
+    <h2 align="center" >ABOUT US</h2>
+    We are a group of passionate people with a vision to disseminate knowledge among students rather teaching. Yellow Alley is fueled by wisdom and motivation of its pioneer faculty members who teaches with all the dedication in their specialized field. The organization has been established with a goal to strengthen the capabilities and empower a student’s practical knowledge through our courses.<br><br>
+    Unlike most of the institutes we are not limited to theory and study materials rather we focus on individual learning by providing practical guidance and hand on experience with real life examples. Established by a team of highly motivated and enthusiastic industry experts, Yellow Alley aspire to make their student’s dream come true.<br><br>
+    We are a group of passionate people who have the potential to understand the context of education era in the education world. Yellow Alley has been formed with an idea to change the concept of education. We are true believers of avant-garde education on the web which results in virality!<br><br>
+    We promote the ideas of innovation and encourage students to learn by exploring and experimenting.  It is the specialization that enables us to guide our students step by step towards overall excellence that leads to success.
+    <h4>Our mission</h4>
+    At Yellow Alley our mission is to educate students like professionals by providing quality education at affordable prices. We are focused to provide full-fledged training to make students self reliant, so that they can confidently apply for relevant jobs in their field. We believe in providing training on practical basis by letting our students understand real life examples. We are continuously working on new technologies to meet the growing needs and provide better learning opportunities to our students.
+    <h4>Our vision</h4>
+    To emerge as a pioneer in the field of education that aims to work for the enrichment of society by helping the students to realize the growing need of advanced technologies in the upcoming education era thereby transforming India as a developed nation. At Yellow Alley, we aim to be the most admired and appreciated brand in the field of education by helping students change their lives through our quality teaching and committed assistance and support throughout and after the training period.
+
 
 </div>
 </body>
