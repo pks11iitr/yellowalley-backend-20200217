4a532f32e23bbc473f213165d7fec764bc54d09b
pankaj sengar <pks11iitr@gmail.com>
2020-05-19 14:30:45 +0530

    contact us form added

--- app/Http/Controllers/Customer/Api/ProfileController.php
+++ app/Http/Controllers/Customer/Api/ProfileController.php
@@ -32,7 +32,7 @@ class ProfileController extends Controller
             'address'=>'required|string|max:150',
             'name'=>'required|string|max:100',
             'email'=>'required|email|max:60',
-            'gender'=>'required|in:male,female,other',
+            'gender'=>'required|in:male,female,others',
             'city'=>'required|string|max:50',
             'pincode'=>'required|string|max:10',
             'qualification'=>'required|string|max:50',
