5b9a4842d368d5f627e5f329c3dbdb45deec8872
pankaj sengar <pks11iitr@gmail.com>
2020-06-03 13:51:32 +0530

    chat plugin

--- resources/views/website/layout.blade.php
+++ resources/views/website/layout.blade.php
@@ -182,7 +182,17 @@
     </div>
 </footer>
 <!-- Footer ends -->
-
+<script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/5e69de8d8d24fc2265872657/default';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+    })();
+</script>
 
 <!-- jQuery first, then Tether, then Bootstrap JS. -->
 <script src="{{asset('js/jquery.min.js')}}"></script>
